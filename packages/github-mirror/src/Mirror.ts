939f87e98040ceb6f9b69155fdb81fe2781a1c7a
Chelsea Urquhart <chelsea.urquhart@rocketsimplicity.com>
2020-03-09 05:00:08 -0700

    github mirroring

---
+++ packages/github-mirror/src/Mirror.ts
@@ -0,0 +1,56 @@
+import {isWebhookPayloadPush} from 'github-webhook-validators';
+import * as childProcess from 'child_process';
+import {dirSync} from 'tmp';
+import * as rimraf from 'rimraf';
+import * as shellEscape from 'shell-escape';
+
+const gitSync = (gitDir: string, ...cmd: Array<string>): void => {
+    const escapedCommand = shellEscape(['git', ...cmd]);
+
+    childProcess.execSync(escapedCommand, {
+        cwd: gitDir,
+        stdio: 'inherit',
+    });
+};
+
+const mkTempDir = (): string => {
+    const tempDir = dirSync({
+        dir: '/tmp',
+    });
+    return tempDir.name;
+};
+
+export class Mirror {
+    async mirrorBranch(branchName: string, exists: boolean, gitDir: string): Promise<void> {
+        console.info(`Mirroring ${branchName}`);
+        gitSync(gitDir, 'version');
+        gitSync(gitDir, 'init');
+        const codeCommitUrl = 'https://git-codecommit.us-west-1.amazonaws.com/v1/repos/ddcp';
+        gitSync(gitDir, 'remote', 'add', 'aws', codeCommitUrl);
+
+        gitSync(gitDir, 'config', `credential.${codeCommitUrl}.helper`, '!/var/task/bin/git-credentials-helper $@');
+        gitSync(gitDir, 'config', `credential.${codeCommitUrl}.UseHttpPath`, 'true');
+
+        if (exists) {
+            gitSync(gitDir, 'remote', 'add', 'github', 'https://github.com/curquhart/ddcp.git');
+            gitSync(gitDir, 'fetch', 'github', branchName);
+            gitSync(gitDir, 'reset', '--hard', `github/${branchName}`);
+            gitSync(gitDir, 'push', 'aws', `HEAD:${branchName}`);
+        }
+        else {
+            gitSync(gitDir, 'push', 'aws', `:${branchName}`);
+        }
+    }
+
+    async handle(event: unknown): Promise<void> {
+        if (isWebhookPayloadPush(event) && event.ref.indexOf('refs/heads/') === 0) {
+            const tmpDir = mkTempDir();
+            try {
+                await this.mirrorBranch(event.ref.replace(/^refs\/heads\//, ''), event.created, tmpDir);
+            }
+            finally {
+                rimraf.sync(tmpDir);
+            }
+        }
+    }
+}
--- packages/publisher/src/PublishPipelineStack.ts
+++ packages/publisher/src/PublishPipelineStack.ts
@@ -1,10 +1,13 @@
-import {App, CfnOutput, Construct, RemovalPolicy, Stack, StackProps} from '@aws-cdk/core';
+import {App, Aws, CfnOutput, Construct, Duration, RemovalPolicy, Stack, StackProps} from '@aws-cdk/core';
 import {Repository} from '@aws-cdk/aws-codecommit';
 import {BlockPublicAccess, Bucket, BucketAccessControl} from '@aws-cdk/aws-s3';
 import {BucketDeployment, Source} from '@aws-cdk/aws-s3-deployment';
+import {LambdaIntegration, PassthroughBehavior, RestApi} from '@aws-cdk/aws-apigateway';
 import {CfnStack} from '@aws-cdk/aws-cloudformation';
 import {createHash} from 'crypto';
 import * as fs from 'fs';
+import {Code, Function, LayerVersion, Runtime} from '@aws-cdk/aws-lambda';
+import {PolicyStatement} from '@aws-cdk/aws-iam';
 
 const app = new App();
 
@@ -50,6 +53,41 @@ class PublisherPipelineInitStack extends Stack {
             }
         });
         managerStack.node.addDependency(deployment);
+
+        const githubMirror = new Function(this, 'GitHubMirror', {
+            code: Code.fromBucket(privateBucket, `${assetsPrefix}@ddcpgithub-mirror.zip`),
+            runtime: Runtime.NODEJS_12_X,
+            handler: 'dist/index.handle',
+            initialPolicy: [
+                new PolicyStatement({
+                    actions: [ 'codecommit:GitPush' ],
+                    resources: [ repo.repositoryArn ],
+                })
+            ],
+            memorySize: 512,
+            timeout: Duration.seconds(30),
+            layers: [
+                // https://github.com/lambci/git-lambda-layer
+                LayerVersion.fromLayerVersionArn(this, 'GitLayer', `arn:aws:lambda:${Aws.REGION}:553035198032:layer:git-lambda2:4`),
+            ]
+        });
+        githubMirror.node.addDependency(deployment);
+
+        const restApi = new RestApi(this, 'WebhookEndpoint');
+        restApi.root.addMethod('POST', new LambdaIntegration(githubMirror, {
+            passthroughBehavior: PassthroughBehavior.WHEN_NO_MATCH,
+            proxy: false,
+            requestParameters: {
+                'integration.request.header.X-Amz-Invocation-Type': '\'Event\'',
+            },
+            integrationResponses: [{
+                statusCode: '202',
+            }]
+        }), {
+            methodResponses: [{
+                statusCode: '202'
+            }]
+        });
 
         new CfnOutput(this, 'PrivateBucketArn', {
             exportName: 'PrivateBucketArn',
